0ae0896eaffcdc4a7c5b4301bcd00800637e5f50
Jean Rojas <jeancarlosrojas12345678@gmail.com>
2024-09-24 07:41:00 -0400

    Add files via upload
    Este es mi proyecto n3

---
+++ proyecto3/RegistrApp/src/app/restablecercontrasena/restablecercontrasena.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restablecercontrasena',
+  templateUrl: './restablecercontrasena.page.html',
+  styleUrls: ['./restablecercontrasena.page.scss'],
+})
+export class RestablecercontrasenaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
